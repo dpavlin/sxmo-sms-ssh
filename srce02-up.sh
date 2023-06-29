7c1de84b55bc9d103d0af2096fa37d6f6c0fac8e
Dobrica Pavlinušić <dpavlin@rot13.org>
2023-06-29 12:39:05 +0200

    remove default route

--- srce02-up.sh
+++ srce02-up.sh
@@ -1,7 +1,15 @@
+#!/bin/sh
+
+# /etc/sudoers.d/user
+#  user    ALL=NOPASSWD: ALL
+sudo ip route del default
+
 nmcli c up Default
+
 while ! ping -c 3 srce02.net.ffzg.hr ; do
 	sleep 1
 done
+
 ssh -N -v -R 22022:localhost:22 dpavlin@srce02.net.ffzg.hr &
 SSH_PID=$!
 echo $SSH_PID | tee /tmp/ssh.pid | logger
