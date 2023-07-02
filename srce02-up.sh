87a1baa96c7b6149a5370403b48e35b96b8a1dda
Dobrica Pavlinušić <dpavlin@rot13.org>
2023-07-02 10:51:18 +0200

    bring up ssh tunnel to srce02

---
+++ srce02-up.sh
@@ -0,0 +1,7 @@
+nmcli c up Default
+while ! ping -c 3 srce02.net.ffzg.hr ; do
+	sleep 1
+done
+ssh -N -v -R 22022:localhost:22 dpavlin@srce02.net.ffzg.hr &
+SSH_PID=$!
+echo $SSH_PID | tee /tmp/ssh.pid | logger
